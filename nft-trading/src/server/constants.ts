2df9b26f0e61e95383568c933d2c460929363f3d
grignaffini <grignaffinipaolo@gmail.com>
2022-09-27 17:27:02 +0200

    added netlify url

--- nft-trading/src/server/constants.ts
+++ nft-trading/src/server/constants.ts
@@ -1,5 +1,5 @@
-export const baseUrl = process.env.NEXT_PUBLIC_VERCEL_URL
-    ? `https://${process.env.NEXT_PUBLIC_VERCEL_URL}`
+export const baseUrl = process.env.NEXT_PUBLIC_NETLIFY_URL
+    ? `https://${process.env.NEXT_PUBLIC_NETLIFY_URL}`
     : 'http://localhost:3000'
 
 
