305c30422e089010b55afdaeb4f151f5c4ef1d99
grignaffini <grignaffinipaolo@gmail.com>
2022-09-27 18:23:48 +0200

    trying to fix cors error

--- nft-trading/src/pages/api/nfts/collection/[contract_address].ts
+++ nft-trading/src/pages/api/nfts/collection/[contract_address].ts
@@ -8,7 +8,6 @@ export default async function handler(req: NextApiRequest, res: NextApiResponse)
     headers.append('Access-Control-Allow-Origin', '*')
     headers.append('Access-Control-Allow-Same-Origin', 'true')
     headers.append('Access-Control-Allow-Methods', 'OPTIONS, HEAD, GET, POST, PUT, DELETE')
-    headers.append('Access-Control-Allow-Credentials', 'true')
     headers.append('Access-Control-Allow-Headers', 'X-Requested-With, X-Auth-Token, Content-Type, Content-Length, Authorization, Access-Control-Allow-Headers, Accept, Access-Control-Allow-Methods, Access-Control-Allow-Origin, Access-Control-Allow-Credentials')
     const address = req.query.contract_address
     const data: any = await fetch(`https://api.nftport.xyz/v0/nfts/${address}?` + new URLSearchParams({
