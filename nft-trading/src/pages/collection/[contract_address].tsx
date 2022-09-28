00620625d432fbf20a7c03eb3f49debc2672e916
grignaffini <grignaffinipaolo@gmail.com>
2022-09-28 15:21:43 +0200

    fixes

--- nft-trading/src/pages/collection/[contract_address].tsx
+++ nft-trading/src/pages/collection/[contract_address].tsx
@@ -10,9 +10,11 @@ function CollectionPage() {
         description: string
         external_url: string
         image_url: string
+        image: string
         name: string
         tokens: number
         unique_owners: number
+        banner?: string
     }
 
     const router = useRouter()
@@ -22,7 +24,7 @@ function CollectionPage() {
     return (
         <div className="min-h-screen">
             <div className='flex flex-col'>
-                <CollectionBanner logo={queryData?.image_url} />
+                <CollectionBanner logo={queryData?.image_url ?? queryData?.image} banner={queryData?.banner} />
                 <div className='bg-white bg-opacity-50 backdrop-blur-xl w-4/5 -mt-32 p-9 lg:p-12 shadow-lg mx-auto rounded-lg'>
                     <CollectionDetails name={queryData.name} description={queryData.description} total={queryData?.tokens} owners={queryData.unique_owners} />
                 </div>
