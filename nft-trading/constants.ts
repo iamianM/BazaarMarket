7cd041268981f4f532dbc2bdc3489cd67d9b2164
Ian McCann <mcian91@gmail.com>
2022-09-30 08:05:43 -0700

    add code

--- nft-trading/constants.ts
+++ nft-trading/constants.ts
@@ -1,6 +1,6 @@
-export const NftTraderRinkeby = "0x4275f11887a9eA761A2dcF8aa9cc3d7A2Db9924A"
-export const NftTraderPolygon = "0x4ee6e8885b684a3de405a3dca83b8823a5acf216"
-export const NftTraderEthereum = "0x657E383EdB9A7407E468acBCc9Fe4C9730c7C275"
-export const DiffemonRinkeby = "0x64c65D71c45e389e686810Fc9d3b9DB748940a26"
-export const DiffemonPolygon = "0xFcafd3545E51Bb52C7b41b6C8734fcD40eF9CD8d"
-export const DiffemonBoosterCode = "ETHGLOBAL2022"
+export const NftTraderRinkeby = "0x4275f11887a9eA761A2dcF8aa9cc3d7A2Db9924A";
+export const NftTraderPolygon = "0x4ee6e8885b684a3de405a3dca83b8823a5acf216";
+export const NftTraderEthereum = "0x657E383EdB9A7407E468acBCc9Fe4C9730c7C275";
+export const DiffemonRinkeby = "0x64c65D71c45e389e686810Fc9d3b9DB748940a26";
+export const DiffemonPolygon = "0xFcafd3545E51Bb52C7b41b6C8734fcD40eF9CD8d";
+export const DiffemonBoosterCodes = ["ethglobal2022", "postthreaddevntell"];
--- nft-trading/src/components/ModalOpenPack.tsx
+++ nft-trading/src/components/ModalOpenPack.tsx
@@ -1,51 +1,75 @@
-import { useEffect } from 'react'
-import CardPack from './CardPack'
-import { useState } from "react"
-import ShowCard from './ShowCard'
-import { useAccount, useNetwork, useProvider } from 'wagmi'
-import { DiffemonRinkeby, DiffemonPolygon, DiffemonBoosterCode } from '../../constants'
-import DiffemonABI from "../abi/DiffemonABI.json"
-import { ethers } from 'ethers'
-import { toast } from 'react-hot-toast'
-import { trpc } from '../utils/trpc'
-import { useSession } from 'next-auth/react'
+import { useEffect } from "react";
+import CardPack from "./CardPack";
+import { useState } from "react";
+import ShowCard from "./ShowCard";
+import { useAccount, useNetwork, useProvider } from "wagmi";
+import {
+  DiffemonRinkeby,
+  DiffemonPolygon,
+  DiffemonBoosterCodes,
+} from "../../constants";
+import DiffemonABI from "../abi/DiffemonABI.json";
+import { ethers } from "ethers";
+import { toast } from "react-hot-toast";
+import { trpc } from "../utils/trpc";
+import { useSession } from "next-auth/react";
 
 function ModalOpenPack() {
+  const [opened, setOpened] = useState(false);
+  const { data: session } = useSession();
+  const { address, isConnected } = useAccount();
+  const [code, setCode] = useState("");
+  const [diffemonContract, setDiffemonContract] =
+    useState<ethers.Contract | null>(null);
+  const [ethersProvider, setEthersProvider] =
+    useState<ethers.providers.Web3Provider>();
+  const [isAllotted, setIsAllotted] = useState(false);
+  const { chain } = useNetwork();
+  const connectedChain = chain?.name.toLowerCase() || "ethereum";
+  const diffemonAddress =
+    connectedChain === "polygon" ? DiffemonPolygon : DiffemonRinkeby;
+  const provider = useProvider();
 
-    const [opened, setOpened] = useState(false)
-    const { data: session } = useSession()
-    const { address, isConnected } = useAccount()
-    const [code, setCode] = useState('')
-    const [diffemonContract, setDiffemonContract] = useState<ethers.Contract | null>(null)
-    const [ethersProvider, setEthersProvider] = useState<ethers.providers.Web3Provider>()
-    const [isAllotted, setIsAllotted] = useState(false)
-    const { chain } = useNetwork()
-    const connectedChain = chain?.name.toLowerCase() || 'ethereum'
-    const diffemonAddress = connectedChain === 'polygon' ? DiffemonPolygon : DiffemonRinkeby
-    const provider = useProvider()
+  // get user from id
+  const { data: user } = trpc.useQuery([
+    "user.get-user",
+    {
+      id: session?.user?.id || "1",
+    },
+  ]);
 
-    // get user from id
-    const { data: user } = trpc.useQuery(['user.get-user', {
-        id: session?.user?.id || '1'
-    }])
+  const updateClaimMutation = trpc.useMutation(["user.update-claim"]);
 
-    const updateClaimMutation = trpc.useMutation(['user.update-claim'])
+  useEffect(() => {
+    const loadContract = async () => {
+      if (typeof window !== "undefined") {
+        const provider: ethers.providers.Provider =
+          new ethers.providers.Web3Provider((window as any).ethereum);
+        const ethprovider: ethers.providers.Web3Provider =
+          new ethers.providers.Web3Provider((window as any).ethereum);
+        await ethprovider.send("eth_requestAccounts", []);
+        setEthersProvider(ethprovider);
+        const signer = new ethers.Wallet(
+          process.env.NEXT_PUBLIC_PACK_ADMIN_PRIVATE_KEY,
+          provider
+        );
+        const diffemonContract: ethers.Contract = new ethers.Contract(
+          diffemonAddress,
+          DiffemonABI,
+          signer
+        );
+        setDiffemonContract(diffemonContract);
+      }
+    };
+    loadContract();
+  }, []);
 
-    useEffect(() => {
-        const loadContract = async () => {
-            if (typeof window !== 'undefined') {
-                const provider: ethers.providers.Provider = new ethers.providers.Web3Provider((window as any).ethereum)
-                const ethprovider: ethers.providers.Web3Provider = new ethers.providers.Web3Provider((window as any).ethereum)
-                await ethprovider.send('eth_requestAccounts', [])
-                setEthersProvider(ethprovider)
-                const signer = new ethers.Wallet(process.env.NEXT_PUBLIC_PACK_ADMIN_PRIVATE_KEY, provider)
-                const diffemonContract: ethers.Contract = new ethers.Contract(diffemonAddress, DiffemonABI, signer)
-                setDiffemonContract(diffemonContract)
-            }
-        }
-        loadContract()
-    }, [])
-
+  const allotBoosters = async () => {
+    const feeData = await provider.getFeeData();
+    await diffemonContract?.allotBoosters(address, 1, {
+      gasPrice: feeData.gasPrice,
+    });
+  };
 
     const allotBoosters = async () => {
         const feeData = await provider.getFeeData()
@@ -53,7 +77,7 @@ function ModalOpenPack() {
     }
 
     const allotPackets = async () => {
-        if (code === DiffemonBoosterCode) {
+        if (DiffemonBoosterCodes.include(code.toLowerCase())) {
             if (!session) {
                 toast.error("You must be logged in to get a packet")
             }
@@ -83,83 +107,124 @@ function ModalOpenPack() {
                 }
             }
         } else {
-            toast.error("You entered a wrong code")
-        }
-    }
-
-    const getCards = async () => {
-        const feeData = await provider.getFeeData()
-        const loadingId = toast.loading("Minting cards...")
-        const signer = ethersProvider?.getSigner(address)
-        const diffemonContractMetamask: ethers.Contract = new ethers.Contract(diffemonAddress, DiffemonABI, signer)
-        try {
-            setIsAllotted(false)
-            await diffemonContractMetamask?.buyBooster({ gasPrice: feeData.gasPrice })
-            toast.success("Cards correctly minted!", {
-                id: loadingId
-            })
-            updateClaimMutation.mutate({ id: user?.id || '' })
-        } catch (e) {
+          const loadingId = toast.loading("Retrieving packet...");
+          try {
+            allotBoosters();
+            setIsAllotted(true);
+            toast.success("Packet correctly retrieved!", {
+              id: loadingId,
+            });
+          } catch (e) {
             toast.error("Ops! Something went wrong, try again later", {
-                id: loadingId
-            })
-            console.error(e)
+              id: loadingId,
+            });
+          }
         }
+      }
+    } else {
+      toast.error("You entered a wrong code");
     }
+  };
 
-    return (
-        <>
-            {/* <label htmlFor="my-modal-6" className="modal-button"> */}
-            <div className='flex flex-col space-y-3 justify-center items-center'>
-                <input type="text" placeholder='Insert your code here...' className='p-5 h-8 placeholder:text-base-100 text-base-100 placeholder:italic rounded-md bg-transparent border shadow-lg outline-none'
-                    onChange={(e) => setCode(e.target.value)} />
-                <div className='shadow-2xl cursor-pointer flex justify-center items-center rounded-full w-32 h-32 bg-gradient-to-bl from-white via-yellow-200 to-secondary'>
-                    <p className='font-poppins text-lg' onClick={() => {
-                        setCode("")
-                        allotPackets()
-                    }}>Claim</p>
+  const getCards = async () => {
+    const feeData = await provider.getFeeData();
+    const loadingId = toast.loading("Minting cards...");
+    const signer = ethersProvider?.getSigner(address);
+    const diffemonContractMetamask: ethers.Contract = new ethers.Contract(
+      diffemonAddress,
+      DiffemonABI,
+      signer
+    );
+    try {
+      setIsAllotted(false);
+      await diffemonContractMetamask?.buyBooster({
+        gasPrice: feeData.gasPrice,
+      });
+      toast.success("Cards correctly minted!", {
+        id: loadingId,
+      });
+      updateClaimMutation.mutate({ id: user?.id || "" });
+    } catch (e) {
+      toast.error("Ops! Something went wrong, try again later", {
+        id: loadingId,
+      });
+      console.error(e);
+    }
+  };
+
+  return (
+    <>
+      {/* <label htmlFor="my-modal-6" className="modal-button"> */}
+      <div className="flex flex-col space-y-3 justify-center items-center">
+        <input
+          type="text"
+          placeholder="Insert your code here..."
+          className="p-5 h-8 placeholder:text-base-100 placeholder:italic rounded-md bg-transparent border shadow-lg outline-none"
+          onChange={(e) => setCode(e.target.value)}
+        />
+        <div className="shadow-2xl cursor-pointer flex justify-center items-center rounded-full w-32 h-32 bg-gradient-to-bl from-white via-yellow-200 to-secondary">
+          <p
+            className="font-poppins text-lg"
+            onClick={() => {
+              setCode("");
+              allotPackets();
+            }}
+          >
+            Claim
+          </p>
+        </div>
+        {isAllotted && (
+          <button className="btn btn-primary" onClick={() => getCards()}>
+            Mint your cards
+          </button>
+        )}
+      </div>
+      {/* </label> */}
+      <input type="checkbox" id="my-modal-6" className="modal-toggle" />
+      <div className="modal">
+        <div className="modal-box w-auto max-w-5xl">
+          <div className="mt-5">
+            {!opened && (
+              <div className="flex flex-col space-y-4">
+                <h3 className="font-bold text-lg text-center">
+                  Click on the pack to open it
+                </h3>
+                <div className="flex justify-center">
+                  <button onClick={() => setOpened(true)}>
+                    <CardPack />
+                  </button>
                 </div>
-                {isAllotted && <button className='btn btn-primary' onClick={() => getCards()}>Mint your cards</button>}
-            </div>
-            {/* </label> */}
-            <input type="checkbox" id="my-modal-6" className="modal-toggle" />
-            <div className="modal">
-                <div className="modal-box w-auto max-w-5xl">
-                    <div className='mt-5'>
-                        {!opened &&
-                            <div className='flex flex-col space-y-4'>
-                                <h3 className="font-bold text-lg text-center">Click on the pack to open it</h3>
-                                <div className='flex justify-center'>
-                                    <button onClick={() => setOpened(true)}>
-                                        <CardPack />
-                                    </button>
-                                </div>
-                            </div>}
-                        {opened &&
-                            (
-                                <div className='flex flex-col space-y-4'>
-                                    <h3 className="font-bold text-lg text-center">Click on the cards to reveal them</h3>
-                                    <div className='grid grid-cols-5 gap-4 p-5'>
-                                        <ShowCard src="/cards/card2.png" />
-                                        <ShowCard src="/cards/card3.png" />
-                                        <ShowCard src="/cards/card4.png" />
-                                        <ShowCard src="/cards/card5.png" />
-                                        <ShowCard src="/cards/card6.png" />
-                                        <ShowCard src="/cards/card7.png" />
-                                        <ShowCard src="/cards/card8.png" />
-                                        <ShowCard src="/cards/card9.png" />
-                                        <ShowCard src="/cards/card10.png" />
-                                        <ShowCard src="/cards/card11.png" />
-                                    </div>
-                                </div>)}
-                    </div>
-                    <div className="modal-action">
-                        <label htmlFor="my-modal-6" className="btn">Done</label>
-                    </div>
+              </div>
+            )}
+            {opened && (
+              <div className="flex flex-col space-y-4">
+                <h3 className="font-bold text-lg text-center">
+                  Click on the cards to reveal them
+                </h3>
+                <div className="grid grid-cols-5 gap-4 p-5">
+                  <ShowCard src="/cards/card2.png" />
+                  <ShowCard src="/cards/card3.png" />
+                  <ShowCard src="/cards/card4.png" />
+                  <ShowCard src="/cards/card5.png" />
+                  <ShowCard src="/cards/card6.png" />
+                  <ShowCard src="/cards/card7.png" />
+                  <ShowCard src="/cards/card8.png" />
+                  <ShowCard src="/cards/card9.png" />
+                  <ShowCard src="/cards/card10.png" />
+                  <ShowCard src="/cards/card11.png" />
                 </div>
-            </div>
-        </>
-    )
+              </div>
+            )}
+          </div>
+          <div className="modal-action">
+            <label htmlFor="my-modal-6" className="btn">
+              Done
+            </label>
+          </div>
+        </div>
+      </div>
+    </>
+  );
 }
 
-export default ModalOpenPack
+export default ModalOpenPack;
